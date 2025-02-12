bd219662274ff60026a6289fb904bddbddc0e618
Krzysztof Lema <lema10@o2.pl>
2025-02-12 21:30:13 +0100

    Initial Commit

---
+++ iAppsTask/iAppsTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAppsTaskApp.swift
+//  iAppsTask
+//
+//  Created by Krzysztof Lema on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iAppsTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iAppsTaskTests/iAppsTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  iAppsTaskTests.swift
+//  iAppsTaskTests
+//
+//  Created by Krzysztof Lema on 12/02/2025.
+//
+
+import Testing
+@testable import iAppsTask
+
+struct iAppsTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
